de9af9693afa2b7863b90f720f0af521205d0beb
Arthur <artur-6272@rambler.ru>
2016-09-10 11:29:10 +0300

    Implement equals

--- src/lesson02/Main.java
+++ src/lesson02/Main.java
@@ -1,10 +1,19 @@
 package lesson02;
 
+import ru.webapp.model.Link;
+
 /**
  * Created by Айслу on 08.09.2016.
  */
 public class Main {
     public static void main(String[] args){
-
+        Link link1 = new Link("JavaWebinar", "javawebinar.ru");
+        Link link2 = link1;
+        Link link3 = new Link(link1);
+        System.out.println(link1.getClass());
+        System.out.println(link1.equals(link2));
+        System.out.println(link1.equals(link3));
+        //link1.out();
+        //link2.out();
     }
 }
