76c42b626a7ce6fe4ef2f31ee2367c2d4ddada68
Arthur <artur-6272@rambler.ru>
2016-09-11 15:06:19 +0300

    ContactType

--- src/ru/webapp/model/Contact.java
+++ src/ru/webapp/model/Contact.java
@@ -4,15 +4,15 @@ package ru.webapp.model;
  * Created by Айслу on 10.09.2016.
  */
 public class Contact {
-    private final String type;
+    private final ContactType type;
     private final String value;
 
-    public Contact(String type, String value) {
+    public Contact(ContactType type, String value) {
         this.type = type;
         this.value = value;
     }
 
-    public String getType() {
+    public ContactType getType() {
         return type;
     }
 
---
+++ src/ru/webapp/model/ContactType.java
@@ -0,0 +1,13 @@
+package ru.webapp.model;
+
+/**
+ * Created by Айслу on 11.09.2016.
+ */
+public enum ContactType {
+    PHONE,
+    MOBILE,
+    HOME_PHONE,
+    SKYPE,
+    MAIL,
+    ICQ
+}
