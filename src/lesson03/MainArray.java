92074c47a5cf7b403cbb6bed59b37615726ef84d
Arthur Kuspakov <artur-6272@rambler.ru>
2016-09-12 16:52:20 +0300

    Add main

---
+++ src/lesson03/MainArray.java
@@ -0,0 +1,12 @@
+package lesson03;
+
+/**
+ * Created by a.kuspakov on 12.09.2016.
+ */
+public class MainArray {
+    public static void main(String[] args) {
+        int[] array = new  int[]{1,2,3};
+        System.out.println(array.length);
+        System.out.println();
+    }
+}
